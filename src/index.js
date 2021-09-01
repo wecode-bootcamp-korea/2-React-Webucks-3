555a74e66616db7f5dbfe4c5eda2d6e96b921ba1
goplanit19@gmail.com <goplanit19@gmail.com>
2021-09-02 07:47:20 +0900

    add: 위벅스 초기화 세팅

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import Router from "./Routes";
+import Router from './Routes';
 
 // Global CSS
-import "./Styles/reset.scss";
+import './styles/reset.scss';
 
 ReactDOM.render(
   <BrowserRouter>
     <Router />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
