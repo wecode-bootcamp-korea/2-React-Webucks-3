c585eff7e138bd16a96b36ce1787693cb539cf72
goplanit19@gmail.com <goplanit19@gmail.com>
2021-09-01 14:32:20 +0900

    fix: eslint & prettier 설정, 초기설정 폴더구조 변경

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import Router from "./Router";
+import Router from "./Routes";
 
 // Global CSS
-import "./index.css";
 import "./Styles/reset.scss";
 
 ReactDOM.render(
