894aef16b77b9de6e6d2bad31c4cdf8eff1f23cb
goplanit19@gmail.com <goplanit19@gmail.com>
2021-09-01 12:51:00 +0900

    Fix: 초기 세팅 완료

---
+++ src/shared/Validation.js
@@ -0,0 +1,27 @@
+//빈칸 체크
+function RequireRule(value) {
+  const require = /.+/;
+  return require.test(value);
+}
+
+// 이메일 체크
+function EmailCheck(email) {
+  const emailCheck =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  return emailCheck.test(email);
+}
+
+// 패스워드 알파벳, 숫자, 특수문자 조합 8자리 이상 체크
+function CheckPassword(pwd) {
+  const checkPwd =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~`!@#$%\^&*()-+=])[A-Za-z\d~`!@#$%\^&*()-+=]{8,}$/;
+  return checkPwd.test(pwd);
+}
+
+const CheckValidation = {
+  RequireRule,
+  EmailCheck,
+  CheckPassword,
+};
+
+export { CheckValidation };
