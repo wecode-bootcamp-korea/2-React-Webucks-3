32bea6fd470f504f11b0e17238f69322b4d91bf3
goplanit19@gmail.com <goplanit19@gmail.com>
2021-09-01 14:32:20 +0900

    fix: eslint & prettier 설정, 초기설정 폴더구조 변경

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import '../src/styles/reset.css';
+=======
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import Router from "./Routes";
+
+// Global CSS
+import "./Styles/reset.scss";
+>>>>>>> c585eff (fix: eslint & prettier 설정, 초기설정 폴더구조 변경)
 
 ReactDOM.render(
   <React.StrictMode>
