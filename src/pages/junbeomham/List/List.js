7be4e111f25c0130dbc85a68dfde821604b0b033
DeVeom <deveomm@gmail.com>
2021-09-01 21:52:26 +0900

    Modify: 미션2, CoffeeCard 컴포넌트 생성 및 mock data 활용하여 List 페이지 구현

--- src/pages/junbeomham/List/List.js
+++ src/pages/junbeomham/List/List.js
@@ -6,6 +6,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 import TopNav from '../../../components/Nav/Nav';
+import COFFEECARD_DATA from './mockData';
+
+class CoffeeCard extends Component {
+  render() {
+    return (
+      <div className="item">
+        <dt className="img-wrap">
+          <div className="scale">
+            <Link to="/detail-junbeom">
+              <img alt={this.props.name} src={this.props.img} />
+            </Link>
+          </div>
+        </dt>
+        <dd>
+          {this.props.name}
+          <button className="like-wrap">
+            <FontAwesomeIcon icon={faHeart} />
+          </button>
+        </dd>
+      </div>
+    );
+  }
+}
 
 class List extends Component {
   render() {
@@ -26,216 +49,15 @@ class List extends Component {
             </section>
             <section className="items-wrap">
               <dl>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <Link to="/detail-junbeom">
-                        <img
-                          alt="sinature the black cold brew"
-                          src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/08/[9200000003661]_20210819094346176.jpg"
-                        />
-                      </Link>
-                    </div>
-                  </dt>
-                  <dd>
-                    시그니처 더 블랙 콜드 브루
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="icecream blending cold brew"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/06/[9200000003643]_20210623101238720.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    아이스크림 블랜딩 콜드 브루
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="nitro vanilla cream"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002487]_20210426091745467.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    나이트로 바닐라 크림
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="nitro cold brew"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000479]_20210426091843897.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    나이트로 콜드 브루
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="dolce cold brew"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000002081]_20210415133656839.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    돌체 콜드 브루
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="vanilla cream cold brew"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000487]_20210430112319040.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    바닐라 크림 콜드 브루
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="velvet dark mocha nitro"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/03/[9200000003509]_20210322093452399.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    벨벳 다크 모카 나이트로
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="forest of kaya in Jeju cold brew"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2020/09/[9200000002672]_20200921171223898.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    제주 비자림 콜드 브루
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="cold brew"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000038]_20210430113202458.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    콜드 브루
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="cold brew malt"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000001636]_20210225093600536.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    콜드 브루 몰트
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="cold brew oat latte"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000003285]_20210416154437069.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    콜드 브루 오트 라떼
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="cold brew float"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/02/[9200000001635]_20210225092236748.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    콜드 브루 플로트
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="french apple tart nitro"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/03/[9200000003507]_20210322093414289.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    프렌치 애플 타르트 나이트로
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
+                {COFFEECARD_DATA[0].map(cardData => {
+                  return (
+                    <CoffeeCard
+                      key={cardData.id}
+                      name={cardData.name}
+                      img={cardData.img}
+                    />
+                  );
+                })}
               </dl>
             </section>
           </article>
@@ -253,38 +75,15 @@ class List extends Component {
             </section>
             <section className="items-wrap">
               <dl>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="ice coffee"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[106509]_20210430111852870.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    아이스 커피
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
-                <div className="item">
-                  <dt className="img-wrap">
-                    <div className="scale">
-                      <img
-                        alt="today cofee"
-                        src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[2]_20210430111934117.jpg"
-                      />
-                    </div>
-                  </dt>
-                  <dd>
-                    오늘의 커피
-                    <button className="like-wrap">
-                      <FontAwesomeIcon icon={faHeart} />
-                    </button>
-                  </dd>
-                </div>
+                {COFFEECARD_DATA[1].map(cardData => {
+                  return (
+                    <CoffeeCard
+                      key={cardData.id}
+                      name={cardData.name}
+                      img={cardData.img}
+                    />
+                  );
+                })}
               </dl>
             </section>
           </article>
