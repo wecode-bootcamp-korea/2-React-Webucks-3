16ee4a3f1d49af7216aab8787305adaa16c33ffa
han0gu <imgum9@gmail.com>
2021-09-03 01:19:17 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/seungwanhan/Login/Login.js
+++ src/pages/seungwanhan/Login/Login.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      idValue: '',
+      pwValue: '',
+      isIdPwValid: false,
+    };
+  }
+
+  handleIdInput = event => {
+    this.setState(
+      {
+        idValue: event.target.value,
+      },
+      this.checkValidation
+    );
+  };
+
+  handlePwInput = event => {
+    this.setState(
+      {
+        pwValue: event.target.value,
+      },
+      this.checkValidation
+    );
+  };
+
+  checkValidation = () => {
+    if (this.state.idValue.indexOf('@') > -1 && this.state.pwValue.length > 4) {
+      this.setState({
+        isIdPwValid: true,
+      });
+    } else {
+      this.setState({
+        isIdPwValid: false,
+      });
+    }
+  };
+
   goToList = () => {
-    console.log(this.props);
-    this.props.history.push('/list');
+    this.props.history.push('/list-seungwan');
   };
 
   render() {
@@ -17,21 +54,27 @@ class Login extends Component {
           </section>
           <form id="userInfo">
             <input
-              id="iptId"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handleIdInput}
             />
-            <input id="iptPw" type="password" placeholder="비밀번호" />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              onChange={this.handlePwInput}
+            />
           </form>
           <button
             id="btnLogin"
+            className={this.state.isIdPwValid ? 'valid' : 'invalid'}
             type="submit"
             form="userInfo"
             onClick={this.goToList}
+            disabled={!this.state.isIdPwValid}
           >
             로그인
           </button>
-          <a id="findPw" href="">
+          <a id="findPw" href="./">
             비밀번호를 잊으셨나요?
           </a>
         </div>
@@ -40,4 +83,4 @@ class Login extends Component {
   }
 }
 
-export default withRouter(Login);
+export default Login;
