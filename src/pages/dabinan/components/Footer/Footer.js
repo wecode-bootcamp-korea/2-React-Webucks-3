39e36fc5921da7ccbb303520f51413bd3c19ee22
dabin <dabin219@naver.com>
2021-09-06 17:22:01 +0900

    리팩토링 완료 1차

--- src/pages/dabinan/components/Footer/Footer.js
+++ src/pages/dabinan/components/Footer/Footer.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 import './Footer.scss';
 
+const FOOTER_DATA = [
+  {
+    id: 'COMPANY',
+    list: [
+      '한눈에 보기',
+      '스타벅스 사명',
+      '스타벅스 소개',
+      '국내 뉴스룸',
+      '세계의 스타벅스',
+      '글로벌 뉴스룸',
+    ],
+  },
+  { id: 'CORPORATE SALES', list: ['단체 및 기업 구매 안내'] },
+  { id: 'PARTNERSHIP', list: ['신규 입점 제의', '협력 고객사 등록 신청'] },
+  {
+    id: 'ONLINE COMMUNITY',
+    list: ['페이스북', '트위터', '유튜브', '블로그', '인스타그램'],
+  },
+  { id: 'RECRUIT', list: ['채용 소개', '채용 지원하기'] },
+  { id: 'WEBUCKS', list: [] },
+];
+
 class Footer extends Component {
   render() {
-    const FOOTER_DATA = [
-      {
-        id: 'COMPANY',
-        list: [
-          '한눈에 보기',
-          '스타벅스 사명',
-          '스타벅스 소개',
-          '국내 뉴스룸',
-          '세계의 스타벅스',
-          '글로벌 뉴스룸',
-        ],
-      },
-      { id: 'CORPORATE SALES', list: ['단체 및 기업 구매 안내'] },
-      { id: 'PARTNERSHIP', list: ['신규 입점 제의', '협력 고객사 등록 신청'] },
-      {
-        id: 'ONLINE COMMUNITY',
-        list: ['페이스북', '트위터', '유튜브', '블로그', '인스타그램'],
-      },
-      { id: 'RECRUIT', list: ['채용 소개', '채용 지원하기'] },
-      { id: 'WEBUCKS', list: [] },
-    ];
     return (
       <footer className="footer">
         {FOOTER_DATA.map((data, id) => {
@@ -31,12 +32,9 @@ class Footer extends Component {
             <div className="footerCategory" key={id}>
               <h3 className="footerListOuter">{data.id}</h3>
               <ul className="footerListInner">
-                <li>{data.list[0]}</li>
-                <li>{data.list[1]}</li>
-                <li>{data.list[2]}</li>
-                <li>{data.list[3]}</li>
-                <li>{data.list[4]}</li>
-                <li>{data.list[5]}</li>
+                {data.list.map((list, id) => {
+                  return <li key={id}>{list}</li>;
+                })}
               </ul>
             </div>
           );
