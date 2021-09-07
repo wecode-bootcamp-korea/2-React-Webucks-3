6907bb8a8c1d2f6efb335527e99b3ebae3d65915
han0gu <imgum9@gmail.com>
2021-09-07 11:13:06 +0900

    Modify: COFFEE_IMG_LIST 변수처리방식 수정

--- src/pages/seungwanhan/List/Grid/Grid.js
+++ src/pages/seungwanhan/List/Grid/Grid.js
@@ -4,15 +4,19 @@ import COFFEE_IMG_LIST from '../data/CoffeeImgList';
 import './Grid.scss';
 
 class Grid extends Component {
-  render() {
-    let imgList = [];
-    if (this.props.category === 'coldbrew') {
-      imgList = COFFEE_IMG_LIST.coldBrewCoffee;
-    } else if (this.props.category === 'brewed') {
-      imgList = COFFEE_IMG_LIST.brewedCoffee;
-    }
+  constructor() {
+    super();
 
-    const gridCell = imgList.map(img => {
+    this.imgList = [];
+    if (this.props.category === 'coldbrew') {
+      this.imgList = COFFEE_IMG_LIST.coldBrewCoffee;
+    } else if (this.props.category === 'brewed') {
+      this.imgList = COFFEE_IMG_LIST.brewedCoffee;
+    }
+  }
+
+  render() {
+    const gridCell = this.imgList.map(img => {
       return <GridCell imgUrl={img.imgUrl} title={img.title} />;
     });
 
