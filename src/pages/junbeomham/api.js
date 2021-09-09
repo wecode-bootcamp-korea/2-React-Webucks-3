4461887c2dd9fb6e2e175bcebcfd795f0c816ab6
DeVeom <deveomm@gmail.com>
2021-09-09 22:56:32 +0900

    Modify: api.js - 각 함수의 async 키워드 삭제

--- src/pages/junbeomham/api.js
+++ src/pages/junbeomham/api.js
@@ -1,18 +1,18 @@
-const getProductsAndSetstate = async component => {
+const getProductsAndSetstate = component => {
   const url = 'http://localhost:3000/data/detailMockData.json';
   fetch(url)
     .then(res => res.json())
     .then(data => component.setState({ product: data.data[0] }));
 };
 
-const getReviewsAndSetstate = async component => {
+const getReviewsAndSetstate = component => {
   const url = 'http://localhost:3000/data/reviewListMockData.json';
   return fetch(url)
     .then(res => res.json())
     .then(data => component.setState({ reviewList: data.reviews }));
 };
 
-const getMenusAndSetstate = async component => {
+const getMenusAndSetstate = component => {
   const url = 'http://localhost:3000/data/menuListMockData.json';
   return fetch(url)
     .then(res => res.json())
